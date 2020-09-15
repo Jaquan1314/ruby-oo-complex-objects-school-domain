cd725b54c37d116ef79e20bf2c4763d788edb2d8
Jaquan1314 <javaughngrant@gmail.com>
2020-09-15 11:23:00 -0400

    Passed all tests

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize (name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student (name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+
+    def grade (student_with_grade)
+        @roster[student_with_grade]
+    end
+
+    def sort
+        roster.each {|key, val| 
+            roster[key] = val.sort
+        }
+    end
+end
